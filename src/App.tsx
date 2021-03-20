047b0e7247362708af94b0d2e57f207f3ae918a1
RafaelKozar <rafael_kozar@hotmail.com>
2021-03-20 12:49:53 -0300

    Init modal new transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard/Dashboard';
 import { Header } from './components/Header/Header';
 import { createServer } from 'miragejs';
 import { GlobalStyle } from './styles/global'
 
 createServer({
-    routes(){
-      this.namespace = 'api';
+  routes() {
+    this.namespace = 'api';
 
-      this.get('/transactions', () => {
-        return [
-          {
-            id: 1,
-            title: 'Transaction 1',
-            amount: 400,
-            type : 'deposit',
-            category: 'Food',
-            createAt : new Date()            
-          }
-        ]
-      });
-    }
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    });
+  }
 });
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
-      <Dashboard />      
-      <GlobalStyle />  
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+
+        <h2>Cadastrar Informarçoes</h2>
+      </Modal>
+      <GlobalStyle />
 
     </>
   );
