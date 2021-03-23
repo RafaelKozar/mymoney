6bdcbe2c88df11a51d382c22c800a0ed851730ef
RafaelKozar <rafael_kozar@hotmail.com>
2021-03-23 14:13:03 -0300

    formatation fields on table

--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -35,9 +35,18 @@ export function TransactionsTalbe() {
                     {transactions.map(transaction => (
                         <tr key={transaction.id}>
                             <td> {transaction.title}</td>
-                            <td className={transaction.type}>R$ {transaction.amount}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td>
                             <td>{transaction.category}</td>
-                            <td>{transaction.createAt}</td>
+                            <td>
+                            {new Intl.DateTimeFormat('pt-BR').format(
+                                new Date(transaction.createAt)
+                            )}
+                            </td>
                         </tr>
                     ))}
 
